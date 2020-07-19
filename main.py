5a683e7edba087ea2bcd024019dc330157efe690
codeandbacon <3091013+codeandbacon@users.noreply.github.com>
2020-07-19 19:07:21 -0400

    add other packet types

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ import sys
 from protocol import ProtocolTest
 
 addr = sys.argv[1]
-data = sys.argv[2]
+try:
+    data = sys.argv[2]
+except:
+    data = None
 
+if data:
+    d = ProtocolTest(addr, 1234)
+    d.send(data, dest=5678)
+else:
+    d = ProtocolTest(addr, 5678)
+    d.listen()
 
-d = ProtocolTest(addr)
+print('===========')
 
-# d.send(data)
-d.listen()
+while True:
+    cmd = input('[rf shell]')
+    cmds = cmd.split(' ')
+    if cmds[0] == 'send':
+        d.send(cmds[1], dest=cmds[2])
+    
+    if cmds[0] == 'echo':
+        d.send('__')
+
+    if cmds[0] == 'neighbors':
+        pass
--- protocol.py
+++ protocol.py
@@ -3,30 +3,62 @@ import threading
 
 class ProtocolTest():
 
-    def __init__(self, addr, baudrate=115200):
-        self.rf = serial.Serial(addr, baudrate)
+    def __init__(self, dev_addr, rf_address=1, baudrate=115200):
+        self.rf = serial.Serial(dev_addr, baudrate)
+        self.address = rf_address
+        self.neighbors = []
+
+    def layer3(self, data):
+        """
+        - actual payload
+        """
+        print('[transport layer] ', data)
+        return
 
     def layer2(self, data):
-        return
+        """
+        - check packet type
+        """
+        pkt_type = data[:2].decode()
+        source_addr = data[2:6]
+        dest_addr = data[6:10]
+        print('[network layer] ', 'packet type ', pkt_type, ' from ', source_addr, ' for ', dest_addr)
+        if pkt_type == '00':
+            self.send('hello from', pkt_type='01')
+            return
+
+        if pkt_type == '01':
+            print('new neighbour', source_addr)
+            # self.send('hello from', pkt_type='01')
+            return
+
+
+        self.layer3(data[10:])
 
     def layer1(self, data):
         """
         - remove total bytes
         - remove signal quality (if present)
         """
-        print(data.decode('utf8'))
-        return
+        data_len = data[0]
+        print('[link layer] ', data_len, ' bytes')
+        self.layer2(data[1:data_len + 1])
 
-    def send(self, data):
+    def send(self, data, dest='0000', pkt_type='00'):
         # TODO: find a better way of sending, AT commands maybe?
-        self.rf.write(bytes(f"send('{data}')\r", 'utf8'))
+        # self.rf.write(bytes(f"t\r", 'utf8'))
+        print('sending', data, dest, pkt_type)
+        source_addr = self.address
+        payload = f'{pkt_type}{source_addr}{dest}{data}'
+        self.rf.write(bytes(f"send('{payload}')\r", 'utf8'))
 
     def recv(self):
         print('waiting data...')
         while True:
             data = self.rf.readline()
-            print('data received', data)
-            self.layer1(data)
+            if data[:3].decode() == 'RCV':
+                self.layer1(data[3:])
+            # self.send('received')
             # return
 
     def listen(self):
